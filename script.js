516171242c8c6a7828400fe6480f81cb81cd3e79
Soundarya K <soundikub@gmail.com>
2021-02-05 23:04:11 +0530

    Add files via upload
    First Commit

---
+++ script.js
@@ -0,0 +1,176 @@
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  //typrewriter effect
+  const words = ["Soundarya.    ", "சௌந்தர்யா.    ", "सौंदर्या|    "];
+  let i = 0;
+  let timer;
+
+  function typingEffect() {
+    let word = words[i].split("");
+    var loopTyping = function () {
+      if (word.length > 0) {
+        document.getElementById("word").innerHTML += word.shift();
+      } else {
+        deletingEffect();
+        return false;
+      }
+      timer = setTimeout(loopTyping, 200);
+    };
+    loopTyping();
+  }
+
+  function deletingEffect() {
+    let word = words[i].split("");
+    var loopDeleting = function () {
+      if (word.length > 0) {
+        word.pop();
+        document.getElementById("word").innerHTML = word.join("");
+      } else {
+        if (words.length > i + 1) {
+          i++;
+        } else {
+          i = 0;
+        }
+        typingEffect();
+        return false;
+      }
+      timer = setTimeout(loopDeleting, 100);
+    };
+    loopDeleting();
+  }
+
+  typingEffect();
+
+  //scroll-to-top function
+  var mybutton = document.getElementById("myBtn");
+  var downButton = document.getElementById("downBtn");
+
+  function scrollFunction() {
+    if (
+      document.body.scrollTop > 620 ||
+      document.documentElement.scrollTop > 620
+    ) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+
+    if (
+      document.body.scrollTop > 70 ||
+      document.documentElement.scrollTop > 70
+    ) {
+      downButton.style.display = "none";
+    } else {
+      downButton.style.display = "block";
+    }
+  }
+
+  window.onscroll = function () {
+    // myFunction();
+    scrollFunction();
+  };
+
+  // "use strict";
+  // //Start of Async Drift Code
+  // !function() {
+  //   var t = window.driftt = window.drift = window.driftt || [];
+  //   if (!t.init) {
+  //     if (t.invoked) return void (window.console && console.error && console.error("Drift snippet included twice."));
+  //     t.invoked = !0, t.methods = [ "identify", "config", "track", "reset", "debug", "show", "ping", "page", "hide", "off", "on" ],
+  //     t.factory = function(e) {
+  //       return function() {
+  //         var n = Array.prototype.slice.call(arguments);
+  //         return n.unshift(e), t.push(n), t;
+  //       };
+  //     }, t.methods.forEach(function(e) {
+  //       t[e] = t.factory(e);
+  //     }), t.load = function(t) {
+  //       var e = 3e5, n = Math.ceil(new Date() / e) * e, o = document.createElement("script");
+  //       o.type = "text/javascript", o.async = !0, o.crossorigin = "anonymous", o.src = "https://js.driftt.com/include/" + n + "/" + t + ".js";
+  //       var i = document.getElementsByTagName("script")[0];
+  //       i.parentNode.insertBefore(o, i);
+  //     };
+  //   }
+  // }();
+  // drift.SNIPPET_VERSION = '0.3.1';
+  // drift.load('pxxbzibkn675');
+  // //End of Async Drift Cod
+
+  $("#downBtnLink").on("click", function (e) {
+    // prevent default anchor click behavior
+    e.preventDefault();
+
+    // store hash
+    var hash = this.hash;
+
+    if ($(hash).length) {
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        50
+      );
+    }
+  });
+
+  const btn = document.querySelector(".theming");
+  const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+  const currentTheme = localStorage.getItem("theme");
+  if (currentTheme == "dark") {
+    document.body.classList.toggle("dark-theme");
+  } else if (currentTheme == "light") {
+    document.body.classList.toggle("light-theme");
+  }
+
+  btn.addEventListener("click", function () {
+    console.log("clicked");
+    if (prefersDarkScheme.matches) {
+      document.body.classList.toggle("light-theme");
+      var theme = document.body.classList.contains("light-theme")
+        ? "light"
+        : "dark";
+    } else {
+      document.body.classList.toggle("dark-theme");
+      var theme = document.body.classList.contains("dark-theme")
+        ? "dark"
+        : "light";
+    }
+    localStorage.setItem("theme", theme);
+  });
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  gsap.from(
+    [
+      "#about-section",
+      "#skills-section",
+      "#work-section",
+      "#project-section",
+      "#contact-section",
+      "#myBtn",
+    ],
+    {
+      duration: 0.8,
+      opacity: 0,
+      scale: 0.1,
+      ease: "power2.out",
+      scrollTrigger: {
+        trigger: [
+          "#about-section",
+          "#skills-section",
+          "#work-section",
+          "#project-section",
+          "#contact-section",
+          "#myBtn",
+        ],
+        // markers: true,
+        toggleActions: "restart none none none",
+      },
+    }
+  );
+});
